b78813f0b889294a44b6ffd6fad203ca26766b64
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-08-29 11:53:58 -0400

    template react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+
 }
 
 export default App;
+
+
+
+// const name = 'John Doe';
+// const showName = false;
+// const loading = false;
+
+// if loading is true show loading, else show Hello (if showName true Hello name)
+{/* {loading ? <h4>Loading ...</h4> : <h1>Hello {showName && name}</h1>} */}
