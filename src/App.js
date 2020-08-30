020e0f1851e0daddc40e1acbe550d7c4b3f7a546
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-08-30 15:30:04 -0400

    moved getUser and getUserRepos functions into context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import React, { Fragment, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import axios from "axios";
 import GithubState from "./context/github/githubState";
 
 import Navbar from "./components/layout/Navbar.jsx";
@@ -14,39 +13,8 @@ import About from "./components/pages/About";
 import "./App.css";
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  // Search Github Users
-
-  //get a single github user
-  const getUser = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/users/${username}?client_id=$
-    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setUser(res.data);
-    setLoading(false);
-  };
-
-  //get user repos
-  const getUserRepos = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
-    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setRepos(res.data);
-    setLoading(false);
-  };
-
-  //Clear users from state
 
   const showAlert = (msg, type) => {
     setAlert({ msg, type });
@@ -79,16 +47,7 @@ const App = () => {
               <Route
                 exact
                 path="/user/:login"
-                render={(props) => (
-                  <User
-                    {...props}
-                    getUser={getUser}
-                    getUserRepos={getUserRepos}
-                    user={user}
-                    repos={repos}
-                    loading={loading}
-                  />
-                )}
+                component={User}
               />
             </Switch>
           </div>
